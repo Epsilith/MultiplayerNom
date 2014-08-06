c0ac53eade763566aee49cd8d6bcce915c33ef87
Yonom <sepehr1376@gmail.com>
2014-08-06 16:26:53 +0200

    More code cleanup

--- MultiplayerNom/LobbyBase.cs
+++ MultiplayerNom/LobbyBase.cs
@@ -2,7 +2,7 @@
 {
     public abstract class LobbyBase<TUser> : Room<TUser>, ILobbyBase where TUser : User, new()
     {
-        protected sealed override bool OnLastUserLeave()
+        protected override sealed bool OnLastUserLeave()
         {
             // Do not close the lobby room even after the last player leaves
             return false;
